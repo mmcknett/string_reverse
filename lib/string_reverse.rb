eac856cfca6962db2a3769dc7d38b4c0e4e9fbdb
Matt McKnett <mmcknett@gmail.com>
2019-03-19 21:19:09 -0700

    [docs] Explain complexity

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,6 +1,14 @@
 # A method to reverse a string in place.
-# Time complexity: ?
-# Space complexity: ?
+# Let n be the length of the input string (my_string)
+# Time complexity:
+#   There are 3 edge case operations. There are 3 operations prior to the loop.
+#   The loop runs n / 2 times.
+#   Each iteration, the loop does 3 operations.
+#   The total time complexity is thus 6 + 3(n/2) = O(n)
+# Space complexity:
+#   The input string is length n. We additionally allocate space for 3 integers.
+#   All changes to the string are done in place, requiring no additional space.
+#   The total space used is on the order of 3 + n = O(n)
 def string_reverse(my_string)
   if my_string == nil || my_string == "" || my_string.length == 1 then
     return my_string
