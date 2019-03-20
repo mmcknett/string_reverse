633bb1bb10807006c0ff329525937b83135f842c
Matt McKnett <mmcknett@gmail.com>
2019-03-19 20:51:46 -0700

    [feat] Implement string_reverse

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -2,5 +2,20 @@
 # Time complexity: ?
 # Space complexity: ?
 def string_reverse(my_string)
-  raise NotImplementedError
+  if my_string == nil || my_string == "" || my_string.length == 1 then
+    return my_string
+  end
+
+  # Until you reach the middle of the string, swap
+  # a character from the front half with a character
+  # from the back half.
+  curr = 0
+  mid = my_string.length / 2
+  last = my_string.length - 1
+  until curr == mid do
+    my_string[curr], my_string[last - curr] = my_string[last - curr], my_string[curr]
+    curr += 1
+  end
+
+  return my_string
 end
